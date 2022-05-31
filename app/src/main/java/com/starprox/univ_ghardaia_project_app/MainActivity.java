fe1cf1287ad04770e845b0359ddd952bc0fac34b
AbderrahmaneSAOUDI <abderrahmane.saoudi.26@gmail.com>
2022-05-31 12:50:31 +0200

    V1.0.3: ** update on [Login]

--- app/src/main/java/com/starprox/univ_ghardaia_project_app/MainActivity.java
+++ app/src/main/java/com/starprox/univ_ghardaia_project_app/MainActivity.java
@@ -1,30 +1,25 @@
 package com.starprox.univ_ghardaia_project_app;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 	private final FirebaseDatabase database = FirebaseDatabase.getInstance ();
 	private final FirebaseAuth auth = FirebaseAuth.getInstance ();
 	private DatabaseReference reference_student, reference_teacher, reference_admin;
-	private Button login_MaterialButton;
 	private TextInputLayout login_cardNumber_TextInputLayout, login_password_TextInputLayout;
 
 	@Override
@@ -33,29 +28,38 @@ public class MainActivity extends AppCompatActivity {
 		setContentView (R.layout.activity_main);
 
 		reference_student = database.getReference (getString (R.string.reference_student));
+		reference_teacher = database.getReference (getString (R.string.reference_teacher));
+		reference_admin = database.getReference (getString (R.string.reference_admin));
 
-		login_MaterialButton = findViewById (R.id.login_MaterialButton);
+		MaterialButton login_MaterialButton = findViewById (R.id.login_MaterialButton);
 		login_cardNumber_TextInputLayout = findViewById (R.id.login_CardNumber_TextInputLayout);
 		login_password_TextInputLayout = findViewById (R.id.login_Password_TextInputLayout);
 
-		login_cardNumber_TextInputLayout.setError (getString (R.string.login_id_card_error));
-		login_password_TextInputLayout.setError (getString (R.string.login_password_error));
-
 		login_MaterialButton.setOnClickListener (view -> {
 			if (verifyLoginFrontend (login_cardNumber_TextInputLayout, login_password_TextInputLayout)) {
+				boolean[] x = verifyLoginBackend (login_cardNumber_TextInputLayout.getEditText ().toString (), login_password_TextInputLayout.getEditText ().toString ());
+				if (x[0] && x[1])
+					Toast.makeText (MainActivity.this, "USERR: setOnClickListener: SUCCESS", Toast.LENGTH_SHORT).show ();
+				else if (! x[0])
+					Toast.makeText (MainActivity.this, "USERR: setOnClickListener: ID Incorrect", Toast.LENGTH_SHORT).show ();
+				else if (! x[1])
+					Toast.makeText (MainActivity.this, "USERR: setOnClickListener: Password Incorrect", Toast.LENGTH_SHORT).show ();
 
-			}
+			} else
+				Toast.makeText (MainActivity.this, "verifyLoginFrontend : Failed", Toast.LENGTH_SHORT).show ();
 		});
 	}
 
 	private boolean verifyLoginFrontend (TextInputLayout _id, TextInputLayout _password) {
 		if (_id.getEditText ().getText ().toString ().length () < 12) {
+			_id.setError (getString (R.string.login_id_card_error));
 			_id.setErrorEnabled (true);
 			return false;
 		} else
 			_id.setErrorEnabled (false);
 
 		if (_password.getEditText ().getText ().toString ().length () < 8) {
+			_password.setError (getString (R.string.login_password_error));
 			_password.setErrorEnabled (true);
 			return false;
 		} else
@@ -67,16 +71,20 @@ public class MainActivity extends AppCompatActivity {
 	// !return  0: Login Success.
 	// !return -1: Card ID doesn't exist in database.
 	// !return -2: password incorrect.
-	private int verifyLoginBackend (String _id, String _password) {
-		ArrayList<Student> students = new ArrayList<> ();
-		boolean id_correct = false;
-		final boolean[] password_correct = { false };
+	private boolean[] verifyLoginBackend (String _id, String _password) {
+		boolean[] id_password = new boolean[2];
+		Query query = reference_student.orderByChild ("cardID").equalTo (_id);
 
-		reference_student.addValueEventListener (new ValueEventListener () {
+		query.addValueEventListener (new ValueEventListener () {
 			@Override
 			public void onDataChange (@NonNull DataSnapshot snapshot) {
-				for (DataSnapshot shot : snapshot.getChildren ())
-					students.add (shot.getValue (Student.class));
+				for (DataSnapshot shot : snapshot.getChildren ()) {
+					Student student = snapshot.getValue (Student.class);
+					if (student.getCardID ().equals (_id)) {
+						id_password[0] = true;
+						return;
+					} else id_password[0] = false;
+				}
 			}
 
 			@Override
@@ -85,20 +93,7 @@ public class MainActivity extends AppCompatActivity {
 			}
 		});
 
-		for (Student item : students) {
-			if (item.getCardID ().equals (_id)) {
-				id_correct = item.getCardID ().equals (_id);
-				auth.signInWithEmailAndPassword (item.getEmailAddress (), _password).addOnCompleteListener (new OnCompleteListener<AuthResult> () {
-					@Override
-					public void onComplete (@NonNull Task<AuthResult> task) {
-						if (task.isSuccessful ()) password_correct[0] = true;
-						else Log.w ("OnCompleteListener:ERROR", task.getException ().toString ());
-					}
-				});
-			}
-		}
 
-
-		return (password_correct[0])? 0: -2;
+		return id_password;
 	}
 }
